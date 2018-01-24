ccc577e1cad60b7d08fcd1c9dbd459be986e8ce5
Ejaz Merchant <ejaz.merchant@icloud.com>
2018-01-23 20:43:52 -0600

    Initial logic for ViewController

--- ProWrestlingQuiz/Controller/ViewController.swift
+++ ProWrestlingQuiz/Controller/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
+    var score: Int = 0
 
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -17,28 +22,99 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressLabel: UILabel!
     
     
-    @IBAction func actionPressed(_ sender: AnyObject) {
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
     }
+    
+    
+    @IBAction func actionPressed(_ sender: AnyObject) {
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        
+        questionNumber = questionNumber + 1
+        
+        
+    }
+    
+    func checkAnswer() {
+        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            
+            score += 1
+
+        } else {
+            
+            print("Wrong!")
+            
+        }
+        
+    }
+    
+    
+    func nextQuestion() {
+        if questionNumber <= 19 {
+            
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+            
+        updateUI()
+            
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Great!",
+                                          message: "You've finished all the questions, do you want to restart?", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart",
+                                              style: .default, handler: {
+                                                (UIAlertAction) in
+                                                self.startOver()
+                                                self.scoreLabel.text = "Score: \(0)"
+            })
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func startOver() {
+        
+        questionNumber = 0
+        nextQuestion()
+        score = 0
+    }
+    
+    func updateUI() {
+        
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / 20"
+        progressBar.frame.size.width = (view.frame.size.width) / 20 * CGFloat(questionNumber + 1)
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 
 
 }
